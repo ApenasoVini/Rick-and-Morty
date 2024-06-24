7b8256c7b01a10a5ab076dc821fb2191d31dbfc1
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2024-06-24 09:12:02 -0300

    API Rick and Morty

--- src/components/CharacterSearch/CharacterSearch.jsx
+++ src/components/CharacterSearch/CharacterSearch.jsx
@@ -1,63 +1,46 @@
 import React, { useState } from 'react';
 import CharacterList from '../CharacterList/CharacterList';
+import './CharacterSearch.scss';
 
 const CharacterSearch = () => {
     const [name, setName] = useState('');
     const [characters, setCharacters] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
 
-    const fetchCharacters = (name) => {
+    const fetchCharacters = async (name) => {
         setLoading(true);
-        fetch(`https://rickandmortyapi.com/api/character/?name=${name}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data.results && data.results.length > 0) {
-                    setCharacters(data.results);
-                    setError(null);
-                } else {
-                    setCharacters([]);
-                    setError('No characters found.');
-                }
-            })
-            .catch(error => {
-                setError(error.message);
-            })
-            .finally(() => {
-                setLoading(false);
-            });
-    };
-
-    const handleChange = (event) => {
-        const inputValue = event.target.value;
-        setName(inputValue.trim());
-
-        if (inputValue.trim() !== '') {
-            fetchCharacters(inputValue);
-        } else {
+        try {
+            const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${name}`);
+            if (!response.ok) throw new Error('Network response was not ok');
+            const data = await response.json();
+            setCharacters(data.results || []);
+        } catch (err) {
             setCharacters([]);
-            setError(null);
+        } finally {
+            setLoading(false);
         }
     };
 
+    const handleChange = (event) => {
+        const inputValue = event.target.value.trim();
+        setName(inputValue);
+        if (inputValue) fetchCharacters(inputValue);
+        else setCharacters([]);
+    };
+
     return (
-        <div>
-            <form>
-                <input
-                    type="text"
-                    placeholder="Escreva o nome..."
-                    value={name}
-                    onChange={handleChange}
-                />
-            </form>
-            {loading && <p>Loading...</p>}
-            {error && <p>Error: {error}</p>}
-            <CharacterList characters={characters} />
+        <div className='CharacterSearch'>
+            <input
+                type="text"
+                placeholder="Escreva o nome..."
+                value={name}
+                onChange={handleChange}
+            />
+            {loading ? (
+                <div className='Spinner'></div>
+            ) : (
+                <CharacterList characters={characters} />
+            )}
         </div>
     );
 };
